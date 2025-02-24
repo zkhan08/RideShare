b850bc1502ac2579be6bbe64c28090a1f60b8a32
Zara Khan <zkhan26@students.polytechnic.org>
2025-02-24 09:11:23 -0800

    Created Person class

---
+++ Tester.java
@@ -0,0 +1,13 @@
+public class Tester {
+
+    public static void main (String[] args){
+        Person a = new Person(5,7);
+        Person b = new Person(2,0);
+        Person c = new Person(10,24);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}

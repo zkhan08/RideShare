4eb816feabc6b8f40ccbe47a5ca8544581a6fb7c
Zara Khan <zkhan26@students.polytechnic.org>
2025-02-24 09:28:31 -0800

    Tester and Car updated code

--- Car.java
+++ Car.java
@@ -1,3 +1,39 @@
+import java.util.*;
+
 public class Car {
+    //fields
+    private int destination;
+    private int currentLocation;
+    private boolean direction;
+    private ArrayList<Person> passangers;
+
+    public Car(int myDestination, int start){
+        destination = myDestination;
+        currentLocation = start;
+        direction = destination > currentLocation;
+        passangers = new ArrayList<Person>();
+    }
+
+    public String toString(){
+        return super.toString() + "Destination: " + destination + "Current location: " + currentLocation + "Going Right: " + direction + "Passangers: " + passangers;
+
+    }
+
+    public void addPassanger(Person p){
+        if(passangers.size() >= 3){
+            System.out.println("ERROR: No room!");
+
+        }else{
+    
+        
+        passangers.add(p);
+
+    }
     
 }
+
+public boolean hasRoom(){
+    return passangers.size() <3 ;
+}
+}
+
--- Tester.java
+++ Tester.java
@@ -8,6 +8,10 @@ public class Tester {
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
+        Car n = new Car(5, 9);
+        System.out.println(n);
+        n.addPassanger(a);
+        System.out.println(n);
     }
     
 }

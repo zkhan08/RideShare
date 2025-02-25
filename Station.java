85f835947396b0f1d81b1070843a0af1cef10b47
Zara Khan <zkhan26@students.polytechnic.org>
2025-02-25 10:24:35 -0800

    Station.java trial

--- Station.java
+++ Station.java
@@ -1,3 +1,31 @@
-public class Station {
+import java.util.ArrayList;
+
+class Station {
+    private ArrayList<Person> waitingPeople;
+
+    public Station(int loc) {
+        waitingPeople = new ArrayList<>();
+    }
     
+    public void addPerson(Person p) {
+        waitingPeople.add(p);
+    }
+    
+    public boolean hasPeopleGoingInDirection(boolean direction) {
+        for (Person p : waitingPeople) {
+            if (p.getDirection() == direction) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public Person getPersonGoingInDirection(boolean direction) {
+        for (int i = 0; i < waitingPeople.size(); i++) {
+            if (waitingPeople.get(i).getDirection() == direction) {
+                return waitingPeople.remove(i);
+            }
+        }
+        return null;
+    }
 }
